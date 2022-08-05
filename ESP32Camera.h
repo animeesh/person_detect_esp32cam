eb8f8e533d977dea56bbbcb17b2d1130ead54651
Animeshkumar Nayak <36356111+animeesh@users.noreply.github.com>
2022-08-05 22:52:10 +0530

    Add files via upload

---
+++ ESP32Camera.h
@@ -0,0 +1,25 @@
+//#define CAMERA_MODEL_M5STACK_WIDE
+#define CAMERA_MODEL_AI_THINKER
+#include <EloquentVision.h>
+
+Eloquent::Vision::ESP32Camera camera;
+camera_fb_t *frame;
+
+
+
+/**
+ * Configure camera
+ */
+void initCamera() {
+  camera.begin(FRAMESIZE_QVGA, PIXFORMAT_GRAYSCALE, 20000000);
+}
+
+
+/**
+ * Capture frame from ESP32 camera
+ */
+uint8_t* captureFrame() {
+  frame = camera.capture();
+
+  return frame->buf;
+}
